6db540d7852fc9db02ac8423e8d389695b4f7e84
Keeverss <keeverss@outlook.com>
2021-03-01 16:04:14 -0500

    Added Files

---
+++ src/com/singh/learningprogramming/Main.java
@@ -0,0 +1,25 @@
+package com.singh.learningprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(area(2));
+        System.out.println(area(-2));
+        System.out.println(area(2, 2));
+        System.out.println(area(-2, 2));
+    }
+
+    public static double area(double radius) {
+        if(radius < 0) {
+            return -1.0;
+        }
+        return Math.PI * Math.pow(radius, 2.00);
+    }
+
+    public static double area(double x, double y) {
+        if(x < 0 || y < 0) {
+            return -1.0;
+        }
+        return x * y;
+    }
+}
